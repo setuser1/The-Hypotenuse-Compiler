6b6046351b814776ca8e1dd0623b99c9106a250e
setuser1 <ss37138@sersd.org>
2025-12-05 16:03:13 -0500

    Add parser.c file and update CMakeLists.txt to include parser source

--- src/Pointers_and_Lines.c
+++
@@ -1,6 +0,0 @@
-//
-// Created by tink on 10/10/25.
-//
-
-#include "compiler.h"
-
---
+++ src/parser.c
@@ -0,0 +1,8 @@
+//
+// Created by tink on 10/10/25.
+//
+
+#include "compiler.h"
+
+void variable_declare(char* array_tok) {}
+void function_declare(char* array_tok) {}
