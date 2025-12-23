94bb23d101208fc73398961c50d3b1c79d22b723
i love femboys <ab37432@sersd.org>
2025-12-23 08:40:47 -0500

    Implement basic Parser class with token handling

--- src/parser.py
+++ src/parser.py
@@ -1,3 +1,28 @@
-from dataclasses import dataclass
-from typing import List, Optional, Any, Tuple
+# parser.py
 from lexer import Lexer, Token
+
+class Parser:
+    def __init__(self, tokens):
+        self.tokens = tokens
+        self.pos = 0
+
+    def peek(self):
+        return self.tokens[self.pos]
+
+    def advance(self):
+        tok = self.tokens[self.pos]
+        self.pos += 1
+        return tok
+
+    def accept(self, kind):
+        if self.peek()[0] == kind:
+            return self.advance()
+        return None
+
+    def expect(self, kind):
+        tok = self.peek()
+        if tok[0] != kind:
+            raise SyntaxError(
+                f"Expected {kind} at position {tok[2]}, got {tok[0]}"
+            )
+        return self.advance()
