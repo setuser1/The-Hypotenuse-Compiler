305f5a6b137ffac74dcfee3567a09c58776fa1ee
setuser1 <ss37138@sersd.org>
2025-12-23 11:55:38 -0500

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,12 +1,13 @@
-from lexer import Token, get_tokens
+from lexer import Tokens, get_tokens
 import sys
 import parser as parse
 
 
-def main(sys.argv):
+def main():
   with open(sys.argv, "r") as file:
     content = file.read()
   tokens = get_tokens(content[0])
   parse.main(tokens)
 
 main()
+
