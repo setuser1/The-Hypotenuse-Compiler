c6fe6bdc1b755f56ed7a7ce0aafc37ecd67c1459
MaimingyouRnlwkey <brnadojhamohj@gmail.com>
2025-12-23 20:31:29 -0500

    lexing, syntax and exception errors

--- src/main.py
+++ src/main.py
@@ -7,7 +7,7 @@ def main():
   if len(sys.argv) != 2:
     print("Usage: python main.py <source-file>")
     sys.exit(1)
-  #Catch file errors with try and except
+  #Catch errors with try and except
   try:
     with open(sys.argv[1], "r") as file:
       content = file.read()
@@ -15,7 +15,13 @@ def main():
     print(f"Error: file not found {sys.argv[1]}")
     sys.exit(1)
   except OSError as e:
-      print(f"Error reading file: {e}")
+    print(f"Error reading file: {e}")
+  except SyntaxError as e:
+    print(f"Syntax error: {e}")
+    sys.exit(1)
+  except Exception as e:
+    print(f"Lexing error: {e}")
+    sys.exit(1)
   tokens = get_tokens(content)
   #Debug: 
   #Add this to verify EOF works: print(tokens[-1])
