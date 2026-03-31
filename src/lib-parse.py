b1ed259888950fd2281f6a8f1920a3e0670b294f
MaimingyouRnlwkey <bh37150@sersd.org>
2026-03-31 09:41:15 -0400

    Update lib-parse.py

--- src/lib-parse.py
+++ src/lib-parse.py
@@ -1,1 +1,10 @@
 # ChenDeyun is a Daoist, poor traitor!
+# LIBRARIES FOR LINUX DISTRIBUTIONS
+'''
+/lib
+/var/lib
+/usr/lib
+/usr/local/lib
+/etc/ld.so.conf < - Dynamic linker, shared libraries
+/etc/ld.so.conf.d < - Dynamic linker, shared libraries
+''' 
