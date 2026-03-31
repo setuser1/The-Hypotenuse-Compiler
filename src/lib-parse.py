0e6edb83a6cd6d7a669c35d6ccf610f17b982301
MaimingyouRnlwkey <bh37150@sersd.org>
2026-03-31 11:50:22 -0400

    Add Windows library paths to lib-parse.py

--- src/lib-parse.py
+++ src/lib-parse.py
@@ -8,3 +8,13 @@
 /etc/ld.so.conf < - Dynamic linker, shared libraries
 /etc/ld.so.conf.d < - Dynamic linker, shared libraries
 ''' 
+# LIBRARIES FOR WINDOWS DISTRIBUTIONS
+'''
+C:\Users\<username>\Documents
+C:\Users\<username>\Music
+C:\Users\<username>\Pictures
+C:\Users\<username>\Videos
+C:\Users\<username>\
+C:\Users\<username>\AppData\Local\Packages\
+C:\Users\<username>\AppData\Roaming\Microsoft\Windows\Libraries
+'''
